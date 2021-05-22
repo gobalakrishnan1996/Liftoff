d28f7d15853ece23eadbc1c5b5ba4f2437db2e29
gobalakrishnan1996 <gobalakrishnangv@gmail.com>
2021-05-22 19:02:18 +0530

    Liftoff

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    count = 50
+    for i in range (count):
+        print (count - i)
+    print ("Liftoff!")
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
